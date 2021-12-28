39a4ba225505e6c838d0bfb9f376f404fa02453a
Mohamed Noufal <getmohamednoufal@gmail.com>
2021-12-28 17:13:19 +0530

    server created, models, routes and controllers created

---
+++ Routes/index.js
@@ -0,0 +1,19 @@
+//require router from the express library
+const express = require("express");
+const router = express.Router();
+
+// import the controllers
+const productsController = require("../Controllers/Products");
+const paymentsController = require("../Controllers/Payments");
+const userController = require("../Controllers/Users");
+const paymentsController = require("../Controllers/Payments");
+
+// declare the routes and bind it with controller functionalities
+router.get("/getAllProducts", productsController.getAllProducts);
+router.post("/login", userController.login);
+router.post("/signup", userController.signup);
+router.post("/payment", paymentsController.payment);
+router.post("/paymentCallback", paymentsController.paymentCallback);
+
+// export the routes
+module.exports = router;
